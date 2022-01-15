b3b46441e642b90d240045e131c752992f9a3ebe
Raul Moldovan <raul.mldv99@gmail.com>
2022-01-15 13:12:00 +0200

    React app setup.

---
+++ mps2-game/src/Dashboard.js
@@ -0,0 +1,7 @@
+import './Dashboard.css'
+
+export default function Dashboard(props) {
+    return(
+        <div>Add dashboar components</div>
+    );
+}
---
+++ mps2-game/src/Header.js
@@ -0,0 +1,7 @@
+import './Header.css'
+
+export default function Header(props) {
+    return(
+        <header>Add app header</header>
+    );
+};
